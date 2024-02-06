ac4b20f308320c1d81e3c9e6bd2ab186e7d95f40
Sean Kim <sehyun.seankim@gmail.com>
2024-02-06 10:59:33 +0900

    Title: 전부 겹치는 선분, Time: 247ms, Memory: 24MB, Status: Passed - CodeTree

--- 240206/전부 겹치는 선분/overlapping-line-segments.py
+++ 240206/전부 겹치는 선분/overlapping-line-segments.py
@@ -4,19 +4,33 @@ input = sys.stdin.readline
 N = int(input())
 segments = [tuple(map(int, input().split())) for _ in range(N)]
 
-
-def solution(N, segments):
-    existing_area = []
-    existing_area.append(segments[0])
-
+def solution(segments):
+    overlapped_area = segments[0]
     for x1, x2 in segments:
-        for start, end in existing_area:
-            if (start <= x1 and x1 <= end) or (start <= x2 and x2 <= end):
-                return "Yes"
-                
-        existing_area.append((x1, x2))
-    
-    return "No"
+        tmp_start, tmp_end = 0, 0
+        start, end = overlapped_area
 
+        if (start <= x1 and x1 <= end):
+            tmp_start = x1
+            if x2 <= end:
+                tmp_end = x2
+            else:
+                tmp_end = end
+        elif x1 < start:
+            tmp_start = x1
+            if x2 < start:
+                return False
+            else:
+                tmp_end = x2
+        # x1 > end
+        else:
+            return False
+        
+        overlapped_area = tmp_start, tmp_end
 
-print(solution(N, segments))
+    return True
+
+if solution(segments):
+    print("Yes")
+else:
+    print("No")
