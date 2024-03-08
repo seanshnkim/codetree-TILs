432323765493a669d2790bb7b9071d06f5032a8c
Sean Kim <sehyun.seankim@gmail.com>
2024-03-08 10:39:04 +0900

    Title: C-TAG, Time: 6122ms, Memory: 26MB, Status: Passed - CodeTree

--- 240308/C-TAG/c-tag.py
+++ 240308/C-TAG/c-tag.py
@@ -19,15 +19,17 @@ for comb in combinations(range(M), 3):
 
     i1, i2, i3 = comb
     for strA in groupA:
-        # 알파벳 순서가 달라진다고 다르게 구분하는가? 그건 아닌 듯.
-        listA = sorted([strA[i1], strA[i2], strA[i3]])
-        tupA = tuple(listA)
+        # 알파벳 순서가 달라진다고 다르게 구분하는가?
+        # listA = sorted([strA[i1], strA[i2], strA[i3]])
+        # tupA = tuple(listA)
+        # setA.add(tupA)
         # tuple, 즉 조합 그 자체를 하나의 원소로 집합에 더해야 한다.
-        setA.add(tupA)
+        setA.add((strA[i1], strA[i2], strA[i3]))
     for strB in groupB:
-        listB = sorted([strB[i1], strB[i2], strB[i3]])
-        tupB = tuple(listB)
-        setB.add(tupB)
+        # listB = sorted([strB[i1], strB[i2], strB[i3]])
+        # tupB = tuple(listB)
+        # setB.add(tupB)
+        setB.add((strB[i1], strB[i2], strB[i3]))
     # 두 집합 간의 교집합이 존재하지 않는다면, 
     # 즉 그룹A와 그룹B가 완벽히 다르게 구분된다면
     if not setA.intersection(setB):
