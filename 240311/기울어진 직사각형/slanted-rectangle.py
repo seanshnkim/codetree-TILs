80a624babfab31cb5d6c2b09437d4619e383e3f6
Sean Kim <sehyun.seankim@gmail.com>
2024-03-11 10:48:26 +0900

    Title: 기울어진 직사각형, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240311/기울어진 직사각형/slanted-rectangle.py
+++ 240311/기울어진 직사각형/slanted-rectangle.py
@@ -30,7 +30,7 @@ def sum_tilted(r, c, right, left):
     r += right
     c -= right
     # 4번 방향의 이동
-    for i in range(left):
+    for i in range(left-1):
         dx, dy = i, i
         cur_sum += grid[r+dx][c+dy]
     
