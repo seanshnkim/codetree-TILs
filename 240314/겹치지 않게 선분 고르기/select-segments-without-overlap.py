698fd4c77d7e3e3ea8650ae0fdb67668a5f2a161
Sean Kim <sehyun.seankim@gmail.com>
2024-03-14 10:14:24 +0900

    Title: 겹치지 않게 선분 고르기, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240314/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240314/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -8,7 +8,7 @@ def intersect(seg1, seg2):
     a1, a2 = seg1
     b1, b2 = seg2
 
-    if a2 <= b1 or b2 <= a1:
+    if a2 < b1 or b2 < a1:
         return False
     else:
         return True
@@ -16,21 +16,29 @@ def intersect(seg1, seg2):
 # 0번째 선분은 기본으로 1개 선택 가능, 따라서 ans는 1부터 시작
 ans = 0
 # 선택한 선분
-selected = [True]*N
-# cnt = 선분의 개수, 인덱스
-def sol(cnt):
-    if cnt == N:
-        return
+selected = [False]*N
+selected[0] = True
+# idx = 선분의 개수, 인덱스
+def sol(idx, cnt):
+    if idx == N:
+        return cnt
 
-    global ans
-    for c in range(cnt):
-        if selected[c] and intersect(segments[c], segments[cnt]):
-            selected[cnt] = False
+    # global ans
+    for c in range(idx):
+        if selected[c] and intersect(segments[c], segments[idx]):
+            selected[idx] = False
             break
-    if selected[cnt]:
-        ans += 1
+    if selected[idx]:
+        cnt += 1
     
-    sol(cnt+1)
+    max_cnt = sol(idx+1, cnt)
+    if selected[idx]:
+        selected[idx] = False
+        max_cnt = max(max_cnt, sol(idx+1, cnt))
+    else:
+        selected[idx] = True
+        max_cnt = max(max_cnt, sol(idx+1, cnt))
+    
+    return max_cnt
 
-sol(0)
-print(ans)
+print(sol(0, 0))
